71f5eefaaa6d747af8ba99d8c0b1da0b4900584a
ronydevdesgn <rodivanio360@gmail.com>
2024-06-27 11:18:40 +0100

    atualizei o arquivo JSON

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routesPages";
+import { Sidebar } from "../../components/Sidebar/Sidebar";
 import "./index.css";
 
 export function Home() {
   return (
     <div className="layout">
-      <h1>Hello! World!</h1>
+      <Sidebar />
       <div className="main-content">
+        <h1>Hello! World!</h1>
         <RouterProvider router={router} />
       </div>
     </div>
