98f51c7e5c925508b54c706030ca0ee81292f113
ronydevdesgn <rodivanio360@gmail.com>
2025-08-15 00:48:14 +0100

    style: Atualização e organização do layout do sistema

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,27 +8,28 @@ export function Sidebar() {
 
   return (
     <div className="container-item sidebar-container">
-      <h1>SWCS</h1>
+      <h1 className="logo-system">SWCS</h1>
+
       <nav>
-        <NavLink to="/cursos">Cursos</NavLink>
-
-        <NavLink to="/dashboard">Dashboard</NavLink>
-
-        {user?.role === "sumarista" && (
-          <>
-            <NavLink to="/professores">Professores</NavLink>
-            <NavLink to="/efetividade">Efetividade</NavLink>
-          </>
-        )}
-        <NavLink to="/relatorios">Relatórios</NavLink>
-
-        <NavLink to="/definicoes">Definições</NavLink>
+        <div className="itens-nav-grup">
+          <NavLink to="/cursos">Cursos</NavLink>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          {user?.role === "sumarista" && (
+            <>
+              <NavLink to="/professores">Professores</NavLink>
+              <NavLink to="/efetividade">Efetividade</NavLink>
+            </>
+          )}
+          <NavLink to="/relatorios">Relatórios</NavLink>
+          <NavLink to="/definicoes">Definições</NavLink>
+        </div>
 
         <div className="sidebar-footer">
-          <hr />
           {/* Editar o nome de Perfil para nome do usuario cadastrado */}
           <NavLink to="/perfil">{user?.name || "Perfil"}</NavLink>
-          <button onClick={logout} className="logout-button">Terminar sessão</button>
+          <button onClick={logout} className="logout-button">
+            Terminar sessão
+          </button>
         </div>
       </nav>
     </div>
