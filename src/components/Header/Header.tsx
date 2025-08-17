c834de01d68e68a68fd0c0bf2ca1fe19d23e728f
ronydevdesgn <rodivanio360@gmail.com>
2025-08-17 19:56:16 +0100

    fix: Correção de erro ao pegar o titulo de cada rota, agora usando o useMatches() para pegar o titulo de cada rota

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import "./Header.css";
 import { useAuth } from "../../hooks/useAuthentication";
 
-export function Header() {
+interface HeaderProps {
+  pageTitle: string;
+}
+
+export function Header({ pageTitle }: HeaderProps) {
   const { user } = useAuth();
 
   return (
     <div className="header-container">
       <div className="user-info">
+        <h1>{pageTitle}</h1>
         <span>Olá, {user?.name}</span>
       </div>
     </div>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useMatches, UIMatch } from "react-router-dom";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import "./Layout.css";
 
+interface RouteHandle {
+  title?: string;
+}
+
 export function Layout() {
+  // aqui o matches já sabe que handle pode ter { title }
+  const matches = useMatches() as UIMatch<unknown, RouteHandle>[];
+  // matches é um array de objetos que representam as rotas correspondentes
+  // cada objeto tem uma propriedade 'handle' que pode conter o título da rota
+
+  // pega o último match (rota mais interna)
+  const currentMatch = matches[matches.length - 1];
+  const pageTitle = currentMatch?.handle?.title || "";
+
   return (
     <div className="layout">
       <Sidebar />
       <div className="container-item main-content">
-        <Header />
+        <Header pageTitle={pageTitle} />
         <main className="container-item">
           <Outlet />
         </main>
